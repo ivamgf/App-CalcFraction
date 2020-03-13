bbe279d466cf769660e8e30814970bae27aa82c6
Ivam <ivam.developer@gmail.com>
2020-03-13 18:36:11 -0300

    First_commit
    First Commit local in project app-CalcFraction

---
+++ appCalcFraction/src/app/views/pages/fraction/fraction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fraction',
+  templateUrl: './fraction.component.html',
+  styleUrls: ['./fraction.component.scss']
+})
+export class FractionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
