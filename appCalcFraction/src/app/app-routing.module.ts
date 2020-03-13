633dde84df0caf2d43ed71d4ef02928ce2bc7447
Ivam <ivam.developer@gmail.com>
2020-03-13 19:15:49 -0300

    Second_commit

--- appCalcFraction/src/app/app-routing.module.ts
+++ appCalcFraction/src/app/app-routing.module.ts
@@ -1,8 +1,58 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoadComponent } from './views/components/load/load.component';
+import { HomeComponent } from './views/pages/home/home.component';
+import { AboutComponent } from './views/pages/about/about.component';
+import { ContactComponent } from './views/pages/contact/contact.component';
+import { TermsComponent } from './views/pages/terms/terms.component';
+import { Page404Component } from './views/components/page404/page404.component';
+import { FractionComponent } from './views/pages/fraction/fraction.component';
+import { TheoryComponent } from './views/pages/theory/theory.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+        redirectTo: 'load',
+        pathMatch: 'full'
+  },
+  {
+    path: 'load',
+    component: LoadComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+      path: 'about',
+      component: AboutComponent
+  },
+  {
+      path: 'contact',
+      component: ContactComponent
+  },
+  {
+      path: 'terms',
+      component: TermsComponent
+  },
+  {
+      path: 'page404',
+      component: Page404Component
+  },
+  {
+    path: '**',
+    component: Page404Component
+  },
+  {
+    path: 'fraction',
+    component: FractionComponent
+  },
+  {
+    path: 'theory',
+    component: TheoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
